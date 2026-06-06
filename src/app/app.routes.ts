18ec2bf46e44d2ccaa939cb8c2ee4a6809909db0
ilyessdoggaz <ilyessdoggaz@example.com>
2026-06-06 23:19:59 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component';
-
 export const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
     title: 'REJUVN | Clean Korean Skincare'
-  },
-  {
-    path: 'privacy',
-    component: PrivacyPageComponent,
-    title: 'REJUVN | Privacy Policy'
   },
   {
     path: '**',
